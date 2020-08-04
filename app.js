a9931b484d6419eaa4c7eab81f5523c7ed8e543f
Prasad <prasad@uniworksdesign.com>
2020-08-04 04:06:55 -0500

    database synch force changed to false

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ ProjectAreaIssueComment.belongsTo(ProjectAreaIssue);
 User.hasMany(ProjectAreaIssueComment, {as: "projectareacomments"});
 ProjectAreaIssueComment.belongsTo(User);
 
-connection.sync({force: true}).then(result=>{
+connection.sync({force: false}).then(result=>{
     console.log("Database synched successfully!!!");
 }).catch(err=>{
     console.log(err);
