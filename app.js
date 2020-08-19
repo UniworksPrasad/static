22e08758d632fd42293835bfb4de457ff54542e3
sapienhwaker <sapienhwaker@gmail.com>
2020-08-19 12:04:50 -0500

    Handled CORS errors

--- app.js
+++ app.js
@@ -349,6 +349,31 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Reqeusted-With, Content-Type, Accept, Authorization');
+  if(req.method === "OPTIONS"){
+    res.header('Access-Control-Allow-Methods','PUT, POST, GET, DELETE, PATCH');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use('/', routes);
+
+app.use((req, res, next) =>{
+  const error = new Error('Endpoint Not Found');
+  error.status= 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error:{
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
